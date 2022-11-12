1915d14eef72d84b4bf1e83598b4219ee168ec6c
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-11-12 15:12:41 +0000

    changed pom config

--- src/main/java/RogueBookings/repositories/UserLogsRepository.java
+++
@@ -1,7 +0,0 @@
-package RogueBookings.repositories;
-
-import RogueBookings.models.userLogs.UserLogs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLogsRepository extends JpaRepository<UserLogs, Long> {
-}
